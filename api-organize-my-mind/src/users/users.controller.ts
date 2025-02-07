3d035e10ab637b4ce7a18b900a799507bb1a1d4a
charles.eduardo.mello.guimaraes@hotmail.com <charles.eduardo.mello.guimaraes@hotmail.com>
2025-02-07 11:51:58 -0300

    refactor: remove Auth annotation because all routes is need auth

--- api-organize-my-mind/src/users/users.controller.ts
+++ api-organize-my-mind/src/users/users.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
     constructor(private usersService: UsersService) { }
 
-    @UseGuards(JwtAuthGuard)
     @Get('me')
     async getProfile(@Request() req) {
         return this.usersService.findById(req.user.id);
