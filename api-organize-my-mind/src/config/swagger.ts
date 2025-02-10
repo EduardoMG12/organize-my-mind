e33f240dc4b8a8071e61063e926f267acb528186
charles.eduardo.mello.guimaraes@hotmail.com <charles.eduardo.mello.guimaraes@hotmail.com>
2025-02-10 09:56:47 -0300

    refacto: change name and description to instance  swagger api

--- api-organize-my-mind/src/config/swagger.ts
+++ api-organize-my-mind/src/config/swagger.ts
@@ -3,8 +3,8 @@ import { INestApplication } from '@nestjs/common';
 
 export function setupSwagger(app: INestApplication) {
     const config = new DocumentBuilder()
-        .setTitle('Minha API')
-        .setDescription('Documentação da API')
+        .setTitle('Organize My Mind API')
+        .setDescription('API documentation for user authentication and management.')
         .setVersion('1.0')
         .addBearerAuth() // Adiciona suporte a autenticação JWT
         .build();
