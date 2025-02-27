fc048384465d9753c545d7bfdfbe039447657e0d
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-02-27 17:39:30 -0300

    refactor: add jest.config outside package.json

---
+++ api-organize-my-mind/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+	moduleDirectories: ["node_modules", "src"],
+	testRegex: ".*\\.spec\\.ts$",
+	transform: {
+		"^.+\\.(t|j)s$": "ts-jest",
+	},
+	collectCoverageFrom: ["**/*.(t|j)s"],
+	coverageDirectory: "../coverage",
+	testEnvironment: "node",
+};
